193d9933281955d26b43835329660c8aeb78612f
César Alberca <cesalberca@gmail.com>
2018-10-17 17:56:45 +0200

    Simplify

--- src/taggedTemplate/__tests__/taggedTemplate.test.js
+++ src/taggedTemplate/__tests__/taggedTemplate.test.js
@@ -1,6 +1,4 @@
 import { taggedTemplate } from '../taggedTemplate'
-import { createObservable, observe } from '../../observable/observable'
-import { flushPromises } from '../../../tests/utils/flushPromises'
 
 describe('taggedTemplate', () => {
   test('retorna el string y un valor ya interpolado', () => {
@@ -21,20 +19,5 @@ describe('taggedTemplate', () => {
     const value2 = 2
     const actual = taggedTemplate`${value1} + ${value2} = ${value1 + value2}`
     expect(actual).toEqual('1 + 2 = 3')
-  })
-
-  test('funciona con observables', async () => {
-    expect.assertions(1)
-    const observable = createObservable({ audience: 'great' })
-    let string = observable.audience
-
-    const observedTemplate = () => {
-      string = taggedTemplate`This audience is ${observable.audience}`
-    }
-    observe(observedTemplate)
-    observable.audience = 'awesome'
-
-    await flushPromises()
-    expect(string).toEqual('This audience is awesome')
   })
 })
